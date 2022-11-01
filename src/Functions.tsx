6871dee02ddae1139eb769a3444e3cd45b31f412
gab5987 <biell.gb4@gmail.com>
2022-11-01 15:15:35 -0300

    Feat: New functions for second part

--- src/Functions.tsx
+++ src/Functions.tsx
@@ -18,9 +18,65 @@ export async function calculateValuesIfSellerDoesNotOwnProperty(props: any) {
     return false;
   }
 
+
+//         + props.isNonResidentialPremisses ? 1 : 0
+//         + props.isFlat ? 1 : 0
+//         + props.isHouse ? 1 : 0
+
   export const calculateValuesIfSellerOwnsProperty = async (props: any) => {
-    alert("entrou")
-  }
+      
+    let totalItens: number = (
+        props.propertyLandPlot + 
+        Number(props.isNonResidentialBuilding) +
+        Number(props.isNonResidentialPremisses) +
+        Number(props.isFlat) +
+        Number(props.isHouse)
+    ) 
+    if(totalItens > 3){
+        totalItens -= 3;
+        totalItens *= 1000;
+    } else totalItens = 0;
+    if(totalItens > 10000){ totalItens = 10000 }
+
+    function calculateValuesIfRealatives(transactionAmount: number): number {
+        if(transactionAmount > 10000000){
+            return 23000 + ((transactionAmount / 100) * 0.1) + 5000 + totalItens;
+        }
+        else return 3000 + ((transactionAmount / 100) * 0.2) + 5000 + totalItens;
+    }
+
+// - if the transaction amount is more than 10,000,000 rubles, then (25,000 rubles + 0.1% of the transaction amount exceeding 10,000,000 rubles, but if the type of property is chosen - an apartment or a residential building, then the tariff is not more than 100,000 rubles) + 6,000 rubles. (if there are more than 3 objects, then starting from the third, 1,000 rubles are added, but not more than 10,000 rubles)
+
+
+    function calculateValuesIfNotRelatives(transactionAmount: number): number {
+        if(transactionAmount <= 1000000) {
+            return 3000 + ((transactionAmount / 100) * 0.4) + 6000 + totalItens;
+        }
+        else if(transactionAmount > 1000000 && transactionAmount <= 10000000) {
+            return 7000 + ((transactionAmount / 100) * 0.2) + 6000 + totalItens;
+        }
+        else if(transactionAmount > 10000000) {
+            transactionAmount = ((transactionAmount / 100) * 0.1);
+            if(transactionAmount > 100000 && !props.isNonResidentialBuilding && !props.isNonResidentialPremisses) {
+                transactionAmount = 100000;
+            }
+            return 25000 + transactionAmount + 6000 + totalItens;
+        }
+        return 0;
+    }
+
+    if(props.areRelatives) {
+        const cadastralValue = calculateValuesIfRealatives(props.propertyCadastralValue);
+        const beingSoldValue = calculateValuesIfRealatives(props.propertyBeingSoldValue);
+
+        alert(`the cadastral value is ${cadastralValue}, and the value of the property being sold is ${beingSoldValue}`)
+    } else {
+        const cadastralValue = calculateValuesIfRealatives(props.propertyCadastralValue);
+        const beingSoldValue = calculateValuesIfRealatives(props.propertyBeingSoldValue);
+
+        alert(`the cadastral value is ${cadastralValue}, and the value of the property being sold is ${beingSoldValue}`)
+    }
+}
   
   export const Navbar = () => {
     return (
